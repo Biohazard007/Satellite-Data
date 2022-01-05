eae5988586a5ea84548232aa86a375236bafb8ee
Dipesh <dipeshmanandhar33@gmail.com>
2022-01-05 17:21:57 +0545

    map is underwork

--- map.py
+++ map.py
@@ -2,7 +2,11 @@ import folium
 
 # create map object
 
-m= folium.Map(location=[27.6557582, 85.3283925], zoom_start=12)
+m = folium.Map(location=[27.6557582, 85.3283925], zoom_start=12)
+
+# Create markers
+folium.Marker([27.6557587, 85.3283922], popup='<strong>Location One</strong>')
+
 # generate map
 m.save('map.html')
 
