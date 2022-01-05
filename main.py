c695b75f8022b0b0a84a57d82f156bb181046f97
Dipesh <dipeshmanandhar33@gmail.com>
2022-01-05 08:16:07 +0545

    Abhishek is fata

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='./static')
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory(path)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/about")
+def hello():
+    return render_template('example.html')
 
  
 
