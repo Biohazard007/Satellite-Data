299847debc5edf0ca4864a74dd8b6a66670ef60e
Dipesh <dipeshmanandhar33@gmail.com>
2022-01-07 18:45:03 +0545

    problem solved

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__,static_folder='./static')
+app = Flask(__name__, static_folder='./static')
 
 
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory(path)
 
+
+@app.route("/")
+def login():
+    return render_template('loading.html')
+
+
 @app.route("/signin")
 def signin():
     return render_template('signin.html')
+
 
 @app.route("/signup")
 def signup():
@@ -20,12 +27,10 @@ def signup():
 def index():
     return render_template('index.html')
 
+
 @app.route("/map")
 def map():
     return render_template('map.html')
 
-@app.route("/")
-def login():
-    return render_template('loading.html')
 
 app.run(debug=True)
