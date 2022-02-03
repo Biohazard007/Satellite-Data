4832d6d83dcc2525601b00edfb5116050b404d38
Rojesh Thapa <thaparojesh65097@gmail.com>
2022-02-03 13:48:10 +0545

    ps

--- ndvi.py
+++ ndvi.py
@@ -1,11 +1,7 @@
-
-
 import glob
 import earthpy.spatial as es
 import earthpy.plot as ep
-
 import rasterio as rio
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -20,15 +16,15 @@ for i in data:
     l.append(f.read(1))
     
 arr_st = np.stack(l)
-
-
+plt.close('all')
 fig1 = ep.plot_rgb(arr_st,
             rgb=(2, 1, 0),
             stretch=True,
             str_clip=0.2,
             figsize=(10, 16)).figure
 
-fig1.savefig("C:/Users/Rojesh Thapa/PycharmProjects/pythonProject/Satellite-Data/Results/RBG.png")
+
+fig1.savefig("C:/Users/Rojesh Thapa/PycharmProjects/pythonProject/Satellite-Data/static/Results/RBG.png")
 
 
 ndvi = es.normalized_diff(arr_st[3], arr_st[2])
@@ -40,6 +36,8 @@ fig2 = ep.plot_bands(ndvi,
                      vmax=1,
                      figsize=(10, 14)).figure
 
-fig2.savefig("C:/Users/Rojesh Thapa/PycharmProjects/pythonProject/Satellite-Data/Results/NDVI.png")
+fig2.savefig("C:/Users/Rojesh Thapa/PycharmProjects/pythonProject/Satellite-Data/static/Results/NDVI.png")
 
-
+import main.py
+main.py.my_func()
+execfile('main.py')
