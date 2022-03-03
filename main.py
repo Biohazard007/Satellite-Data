39de81e33558b1ab8a85fa298e1ef1c99afe8440
dipesh <dipeshmanandhar33@gmail.com>
2022-03-03 13:50:34 +0545

    ndvi done

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, redirect, render_template, send_from_directory, request
 import geemap.foliumap as geemap
 import ee
 import os
 import geopandas as gpd
 import uuid
 
+
 try:
-        ee.Initialize()
+    ee.Initialize()
 except Exception as e:
-        ee.Authenticate()
-        ee.Initialize()
+    ee.Authenticate()
+    ee.Initialize()
+
 
 def uploaded_file_to_gdf(data):
     import tempfile
@@ -34,6 +36,7 @@ def uploaded_file_to_gdf(data):
 
 app = Flask(__name__, static_folder='./static')
 
+
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory(path)
@@ -41,7 +44,7 @@ def send_js(path):
 
 @app.route("/")
 def login():
-    return render_template('loading.html')
+    return render_template('index.html')
 
 
 @app.route("/signin")
@@ -53,26 +56,26 @@ def signin():
 def signup():
     return render_template('signup.html')
 
-
-@app.route("/index")
-def index():
-    return render_template('index.html')
+@app.route("/upload")
+def upload_page():
+    return render_template('upload.html')
 
 @app.route("/timelapse")
 def timelapse():
-    if not os.path.exists(os.getcwd()+'/static/ignore/base.html'):
+    if not os.path.exists(os.getcwd() + '/static/ignore/base.html'):
         m = geemap.Map(
-                basemap="HYBRID",
-                plugin_Draw=True,
-                draw_export=True,
-                locate_control=True,
-                plugin_LatLngPopup=False,
-            )
+            basemap="HYBRID",
+            plugin_Draw=True,
+            draw_export=True,
+            locate_control=True,
+            plugin_LatLngPopup=False,
+        )
         m.add_basemap("ROADMAP")
-        m.save(os.getcwd()+'/static/ignore/base.html')
-    return render_template('timelapse.html',map="base.html")
+        m.save(os.getcwd() + '/static/ignore/base.html')
+    return render_template('timelapse.html', map="base.html")
 
-@app.route('/geotogif',methods=['POST'])
+
+@app.route('/geotogif', methods=['POST'])
 def geotogif():
     rand_name = uuid.uuid4()
     file_name = request.form['file']
@@ -80,86 +83,103 @@ def geotogif():
     # check condition if file exists
     gdf = gpd.read_file(geo_file)
     roi = geemap.geopandas_to_ee(gdf, geodesic=False)
-    path_to_save = os.getcwd()+f'/static/ignore/{rand_name}.gif'
+    path_to_save = os.getcwd() + f'/static/ignore/{rand_name}.gif'
 
     collection = request.form.get('collection', 'landsat')
-    start_year = request.form.get('start_date',1984)
-    end_year = request.form.get('end_date',2021)
-    bands = request.form.get('bands','Red/Green/Blue').split('/')
-    print('bands',bands)
-    frequency = request.form.get('frequency','year')
+    start_year = request.form.get('start_date', 1984)
+    end_year = request.form.get('end_date', 2021)
+    bands = request.form.get('bands', 'Red/Green/Blue').split('/')
+    print('bands', bands)
+    frequency = request.form.get('frequency', 'year')
     if collection == 'landsat':
         out_gif = geemap.landsat_timelapse(
-                roi=roi,
-                out_gif =path_to_save,
-                start_year=int(start_year or 1984),
-                end_year=int(end_year or 2021),
-                start_date='01-01',
-                end_date='12-30',
-                bands=bands,
-                apply_fmask=True,
-                date_format=None,
-                title='Landsat Timelapse',
-                frames_per_second=5,
-                dimensions=768,
-                frequency=frequency,
-                title_xy=("2%", "90%"),
-                add_text=True,
-                text_xy=("2%", "2%"),
-                text_sequence=None,
-                add_progress_bar=True,
-                progress_bar_color='#0000ff',
-                progress_bar_height=5,
-                loop=0,
-            )
+            roi=roi,
+            out_gif=path_to_save,
+            start_year=int(start_year or 1984),
+            end_year=int(end_year or 2021),
+            start_date='01-01',
+            end_date='12-30',
+            bands=bands,
+            apply_fmask=True,
+            date_format=None,
+            title='Landsat Timelapse',
+            frames_per_second=5,
+            dimensions=768,
+            frequency=frequency,
+            title_xy=("2%", "90%"),
+            add_text=True,
+            text_xy=("2%", "2%"),
+            text_sequence=None,
+            add_progress_bar=True,
+            progress_bar_color='#0000ff',
+            progress_bar_height=5,
+            loop=0,
+        )
     else:
         out_gif = geemap.sentinel2_timelapse(
-                roi=roi,
-                out_gif =path_to_save,
-                start_year=int(start_year or 2015),
-                end_year=int(end_year or 2021),
-                start_date='01-01',
-                end_date='12-30',
-                bands=bands,
-                apply_fmask=True,
-                date_format=None,
-                title='Sentinel Timelapse',
-                frames_per_second=5,
-                dimensions=768,
-                frequency=frequency,
-                title_xy=("2%", "90%"),
-                add_text=True,
-                text_xy=("2%", "2%"),
-                text_sequence=None,
-                add_progress_bar=True,
-                progress_bar_color='#0000ff',
-                progress_bar_height=5,
-                loop=0,
-            )
+            roi=roi,
+            out_gif=path_to_save,
+            start_year=int(start_year or 2015),
+            end_year=int(end_year or 2021),
+            start_date='01-01',
+            end_date='12-30',
+            bands=bands,
+            apply_fmask=True,
+            date_format=None,
+            title='Sentinel Timelapse',
+            frames_per_second=5,
+            dimensions=768,
+            frequency=frequency,
+            title_xy=("2%", "90%"),
+            add_text=True,
+            text_xy=("2%", "2%"),
+            text_sequence=None,
+            add_progress_bar=True,
+            progress_bar_color='#0000ff',
+            progress_bar_height=5,
+            loop=0,
+        )
     print(out_gif)
     return {
         'gif': f'/static/ignore/{rand_name}.gif'
     }
 
-@app.route('/upload-geojson',methods=['POST'])
+
+@app.route('/upload-geojson', methods=['POST'])
 def upload():
     rand_name = uuid.uuid4()
     m = geemap.Map(
-            basemap="HYBRID",
-            plugin_Draw=True,
-            draw_export=True,
-            locate_control=True,
-            plugin_LatLngPopup=False,
-        )
+        basemap="HYBRID",
+        plugin_Draw=True,
+        draw_export=True,
+        locate_control=True,
+        plugin_LatLngPopup=False,
+    )
     m.add_basemap("ROADMAP")
     geo_file = request.files['file']
     geo_file.save(f'upload/{rand_name}.geojson')
     gdf = uploaded_file_to_gdf(geo_file)
     m.add_gdf(gdf, "ROI")
-    m.save(os.getcwd()+f'/static/ignore/{rand_name}.html')
+    m.save(os.getcwd() + f'/static/ignore/{rand_name}.html')
     return {
         'map': f'/static/ignore/{rand_name}.html',
         'file': f'{rand_name}'
     }
+
+@app.route('/result', methods=['POST'])
+def handle_upload_image():
+    if 'files[]' not in request.files:
+        return redirect('/upload')
+    files = request.files.getlist('files[]')
+    file_names = []
+    for file in files:
+        filename =  f'{uuid.uuid4()}.tiff'
+        file_names.append(filename)
+        file.save(f'static/ignore/{filename}')
+    return render_template('upload.html', filenames=file_names)
+
+
+
+
 
 app.run(debug=True)
--- ndvi.py
+++ ndvi.py
@@ -1,47 +1,96 @@
-import glob
-import earthpy.spatial as es
-import earthpy.plot as ep
+
+# pip install rasterio
+# pip install earthpy
+
 import rasterio as rio
+from rasterio import plot
 import matplotlib.pyplot as plt
 import numpy as np
-class ndvi:
-        def __init__(self, landsat, data):
-            self.landsat = landsat
-            self.data = data
+import glob
+from matplotlib.colors import ListedColormap
+import earthpy.plot as ep
 
-
-landsat = 'static/Temp/*.tiff'
-data = glob.glob(landsat)
+location = 'static/Data/process/*'
+data = glob.glob(location)
 data.sort()
+data
 
-l = []
-
+l=[]
 for i in data:
-    with rio.open(i, 'r') as f:
-        l.append(f.read(1))
-
+  with rio.open(i, 'r') as f:
+    l.append(f.read(1))
 arr_st = np.stack(l)
-plt.close('all')
+
 fig1 = ep.plot_rgb(arr_st,
-                   rgb=(2, 1, 0),
-                   stretch=True,
-                   str_clip=0.2,
-                   figsize=(4, 8)).figure
+            rgb=(2, 1, 0),
+            stretch=True,
+            str_clip=0.2,
+            figsize=(10, 16)).figure
+
 
 fig1.savefig("static/Results/RBG.png")
 
-ndvi = es.normalized_diff(arr_st[3], arr_st[2])
+fig2 = ep.plot_rgb(arr_st,
+            rgb=(3, 2, 1),
+            stretch=True,
+            str_clip=0.2,
+            figsize=(10, 16)).figure
+fig2.savefig("static/Results/False_Color.png")
 
-fig2 = ep.plot_bands(ndvi,
-                     cmap="RdYlGn",
-                     cols=1,
-                     vmin=-1,
-                     vmax=1,
-                     figsize=(4, 8)).figure
+red = l[2].astype('float64')
+nir = l[3].astype('float64')
 
-fig2.savefig("static/Results/NDVI.png")
+ndvi = np.where(
+    (red + nir)==0.,
+    0,
+    (nir - red)/(nir + red)
+)
 
-#import main.py
 
-#main.py.my_func()
-#execfile('main.py')
+ndvi_class_bins = [-1.0, -0.5, 0, 0.2, 0.4, 0.6, 0.8, 1.0]
+ndvi_landsat_class = np.digitize(ndvi, ndvi_class_bins)
+
+# Apply the nodata mask to the newly classified NDVI data
+ndvi_landsat_class = np.ma.masked_where(
+    np.ma.getmask(ndvi), ndvi_landsat_class
+)
+np.unique(ndvi_landsat_class)
+
+# Define color map
+# nbr_colors = ["#13128f", "#96601a", "#670f00", "#38bd09", "#013407"]
+nbr_colors = ["#13128f", "#eab64f", "#926829", "#3e3117", "#38bd09", "#008b3a", "#013407"]
+# nbr_colors = ["#13128f", "#96601a", "#670f00", "#38bd09", "#013407"]
+nbr_cmap = ListedColormap(nbr_colors)
+
+# Define class names
+ndvi_cat_names = [
+    "Water Bodies",
+    "No Vegetation",
+    "Bare Area",
+    "Low Vegetation",
+    "Moderate Vegetation",
+    "Good Vegetation",
+    "High Vegetation",
+]
+
+# Get list of classes
+classes = np.unique(ndvi_landsat_class)
+classes = classes.tolist()
+# The mask returns a value of none in the classes. remove that
+classes = classes[0:7]
+
+# Plot your data
+fig, ax = plt.subplots(figsize=(12, 12))
+im = ax.imshow(ndvi_landsat_class, cmap=nbr_cmap)
+
+ep.draw_legend(im_ax=im, classes=classes, titles=ndvi_cat_names)
+ax.set_title(
+    "Normalized Difference Vegetation Index (NDVI) Classes",
+    fontsize=14,
+)
+ax.set_axis_off()
+
+# Auto adjust subplot to fit figure size
+plt.tight_layout()
+
+plt.savefig('static/Results/ndvi.png')
